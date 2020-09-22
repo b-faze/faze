2704f01344c27c77587f5f905b1f9b5498a97b3f
b-hub <ben.magistris@gmail.com>
2020-09-22 08:28:19 +0100

    Add Engine classes and nuget configuration

---
+++ Faze.Engine/Simulators/GameSimulator.cs
@@ -0,0 +1,32 @@
+﻿using Faze.Abstractions.Engine;
+using Faze.Abstractions.GameStates;
+using Faze.Abstractions.Players;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Faze.Engine.Simulators
+{
+    public class GameSimulator : IGameSimulator
+    {
+        public TResult Simulate<TMove, TResult>(IGameState<TMove, TResult, IPlayer> state)
+        {
+            TResult result;
+            while ((result = state.Result) == null)
+            {
+                var move = state.CurrentPlayer.ChooseMove(state);
+                state = state.Move(move);
+            }
+
+            return result;
+        }
+
+        public IEnumerable<TResult> Sample<TMove, TResult>(IGameState<TMove, TResult, IPlayer> state, int n)
+        {
+            while (n-- > 0)
+            {
+                yield return Simulate(state);
+            }
+        }
+    }
+}
---
+++ Faze.Engine/Utilities/ThreadSafeRandom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Faze.Engine.Utilities
+{
+    public static class ThreadSafeRandom
+    {
+        private static RNGCryptoServiceProvider _global = new RNGCryptoServiceProvider();
+
+        [ThreadStatic]
+        private static Random _local;
+
+        public static Random Random()
+        {
+            Random inst = _local;
+            if (inst == null)
+            {
+                byte[] buffer = new byte[4];
+                _global.GetBytes(buffer);
+                _local = inst = new Random(BitConverter.ToInt32(buffer, 0));
+            }
+
+            return inst;
+        }
+    }
+}
