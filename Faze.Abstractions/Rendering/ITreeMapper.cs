392960afeba6782bf561bcdbd37411e8f0f14fba
b-hub <ben.magistris@gmail.com>
2020-09-23 19:45:28 +0100

    add more rendering interfaces

---
+++ Faze.Abstractions/Rendering/ITreeMapper.cs
@@ -0,0 +1,9 @@
+﻿using Faze.Abstractions.Core;
+
+namespace Faze.Abstractions.Rendering
+{
+    public interface ITreeMapper<TValueIn, TValueOut>
+    {
+        Tree<TValueOut> Map(Tree<TValueIn> tree);
+    }
+}
---
+++ Faze.Abstractions/Rendering/ITreePainter.cs
@@ -0,0 +1,14 @@
+﻿using Faze.Abstractions.Core;
+
+namespace Faze.Abstractions.Rendering
+{
+    public interface ITreePainter
+    {
+        PaintedTree Paint<T>(Tree<T> tree);
+    }
+
+    public interface ITreePainter<TValue>
+    {
+        PaintedTree Paint(Tree<TValue> tree);
+    }
+}
