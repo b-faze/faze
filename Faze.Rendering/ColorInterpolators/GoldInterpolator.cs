5ae94dff696dda1937c85aa702532556a3df1752
b-hub <ben.magistris@gmail.com>
2020-09-24 08:29:26 +0100

    add interpolator tests and a wiki/images folder

---
+++ Faze.Rendering/ColorInterpolators/GoldInterpolator.cs
@@ -0,0 +1,19 @@
+﻿using Faze.Abstractions.Rendering;
+using System;
+using System.Drawing;
+
+namespace Faze.Rendering.ColorInterpolators
+{
+    public class GoldInterpolator : IColorInterpolator
+    {
+        public Color GetColor(double d)
+        {
+            var drawOffset = 1 - Math.Abs(0.5 - d) / 0.5;
+            var r = (int)(255 * d + drawOffset * 127);
+            var g = (int)(drawOffset * 255);
+            var b = (int)(255 * (1 - d) - drawOffset * 127);
+
+            return Color.FromArgb(255, r, g, b);
+        }
+    }
+}
