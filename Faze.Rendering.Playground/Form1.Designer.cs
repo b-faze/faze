85360c8f2b33d519984bf26e9917b72bc8f5279c
b-hub <ben.magistris@gmail.com>
2020-10-14 21:59:52 +0100

    setup for Windows Forms 'Playground' for playing around with tree renderers

---
+++ Faze.Rendering.Playground/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Faze.Rendering.Playground
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.renderBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(480, 311);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // renderBtn
+            // 
+            this.renderBtn.Location = new System.Drawing.Point(537, 13);
+            this.renderBtn.Name = "renderBtn";
+            this.renderBtn.Size = new System.Drawing.Size(75, 23);
+            this.renderBtn.TabIndex = 1;
+            this.renderBtn.Text = "button1";
+            this.renderBtn.UseVisualStyleBackColor = true;
+            this.renderBtn.Click += new System.EventHandler(this.renderBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.renderBtn);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button renderBtn;
+    }
+}
+
---
+++ Faze.Rendering.Playground/Form1.cs
@@ -0,0 +1,41 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faze.Rendering.Playground
+{
+    public partial class Form1 : Form
+    {     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void renderBtn_Click(object sender, EventArgs e)
+        {
+            SKImageInfo imageInfo = new SKImageInfo(300, 250);
+
+            using (SKSurface surface = SKSurface.Create(imageInfo))
+            {
+                var canvas = surface.Canvas;
+                canvas.DrawColor(SKColors.Red);
+
+                using (SKImage image = surface.Snapshot())
+                using (SKData data = image.Encode(SKEncodedImageFormat.Png, 100))
+                using (MemoryStream mStream = new MemoryStream(data.ToArray()))
+                {
+                    Bitmap bm = new Bitmap(mStream, false);
+                    pictureBox.Image = bm;
+                }
+            }
+        }
+    }
+}
