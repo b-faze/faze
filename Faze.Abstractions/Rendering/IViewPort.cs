27371156a1a0b08462b9933777f1a9b1d160eaaa
b-hub <ben.magistris@gmail.com>
2020-12-30 13:42:44 +0000

    simplify IViewPort and create a corresponding struct. ViewPort is assumed to have the same dimentions as the parent image

--- Faze.Abstractions/Rendering/IViewPort.cs
+++ Faze.Abstractions/Rendering/IViewPort.cs
@@ -1,11 +1,42 @@
-﻿namespace Faze.Abstractions.Rendering
+﻿using System.Net;
+
+namespace Faze.Abstractions.Rendering
 {
     public interface IViewPort
     {
         float Left { get; }
         float Top { get; }
-        float Width { get; }
-        float Height { get; }
         float Scale { get; }
+    }
+
+    public struct ViewPort : IViewPort
+    {
+        public ViewPort(float left, float top, float scale)
+        {
+            Left = left;
+            Top = top;
+            Scale = scale;
+        }
+
+        public static ViewPort Default()
+        {
+            return new ViewPort(0, 0, 1);
+        }
+
+        /// <summary>
+        /// Relative position as a fraction of the image's width
+        /// </summary>
+        public float Left { get; }
+
+        /// <summary>
+        /// Relative position as a fraction of the image's height
+        /// </summary>
+        public float Top { get; }
+
+        /// <summary>
+        /// Relative scale of the viewport compared to the image's size
+        /// e.g. Scale = 0.5 would be a view port half the size of the original image
+        /// </summary>
+        public float Scale { get; }
     }
 }
