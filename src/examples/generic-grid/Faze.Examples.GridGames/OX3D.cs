bbe9ba00ae09a1d8603defff96136c31c646e3cd
b-hub <ben.magistris@gmail.com>
2021-07-07 08:07:13 +0100

    add more generic grid games

---
+++ src/examples/generic-grid/Faze.Examples.GridGames/OX3D.cs
@@ -0,0 +1,160 @@
+﻿using Faze.Abstractions.GameMoves;
+using Faze.Abstractions.GameResults;
+using Faze.Abstractions.GameStates;
+using Faze.Abstractions.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Faze.Examples.GridGames
+{
+    public class OX3D : IGameState<GridMove, WinLoseDrawResult?>
+    {
+        public int Dimention => 3;
+        public int TotalPlayers => 2;
+        public PlayerIndex CurrentPlayerIndex => p1Turn ? PlayerIndex.P1 : PlayerIndex.P2;
+
+        private List<List<int[]>> winningStates;
+        private bool p1Turn = true;
+        private bool?[,,] pMoves;
+        private List<GridMove> availableMoves;
+
+        public OX3D()
+        {
+            pMoves = new bool?[Dimention, Dimention, Dimention];
+            availableMoves = Enumerable.Range(0, 9).SelectMany(x => Enumerable.Repeat(x, Dimention)).Select(x => new GridMove(x)).ToList();
+            winningStates = GetWinningStates(Dimention).ToList();
+        }
+
+        private OX3D(bool?[,,] pMoves, List<GridMove> availableMoves, bool p1Turn, List<List<int[]>> winningStates)
+        {
+            this.pMoves = (bool?[,,])pMoves.Clone();
+            this.p1Turn = p1Turn;
+            this.availableMoves = availableMoves.ToList();
+            this.winningStates = winningStates;
+        }
+
+        public IEnumerable<GridMove> GetAvailableMoves()
+        {
+            return availableMoves.Distinct().ToList();
+        }
+
+        public IGameState<GridMove, WinLoseDrawResult?> Move(GridMove move)
+        {
+            var moveX = move % Dimention;
+            var moveY = move / Dimention;
+            var i = 0;
+            while (pMoves[moveX, moveY, i].HasValue)
+                i++;
+
+            var newPMoves = new bool?[Dimention,Dimention,Dimention];
+            var newAvailableMoves = availableMoves.ToList();
+
+            pMoves.CopyTo(newPMoves, 0);
+
+            newPMoves[moveX, moveY, i] = p1Turn;
+            newAvailableMoves.Remove(move);
+
+            return new OX3D(newPMoves, newAvailableMoves, !p1Turn, winningStates);
+        }
+
+        public WinLoseDrawResult? GetResult()
+        {
+            foreach (var state in winningStates)
+            {
+                if (state.All(s => pMoves[s[0], s[1], s[2]] == true)) return WinLoseDrawResult.Win;
+                if (state.All(s => pMoves[s[0], s[1], s[2]] == false)) return WinLoseDrawResult.Lose;
+            }
+
+            return availableMoves.Count == 0 ? WinLoseDrawResult.Draw : (WinLoseDrawResult?)null;
+        }
+
+        public static IEnumerable<List<int[]>> GetWinningStates(int dimention)
+        {
+            var directions = GetDirections(dimention);
+            var states = directions.SelectMany(GetStates);
+            return states.Select(l => l.ToList());
+        }
+
+        public static IEnumerable<List<int[]>> GetStates(int[] direction)
+        {
+            var freedom = direction.Count(x => x == 0);
+            var dimention = direction.Length;
+            if (freedom == 0)
+            {
+                var res = new List<int[]>();
+                for (var i = 0; i < dimention; i++)
+                {
+                    res.Add(direction.Select((d, di) => TransformDirection(direction, di, dimention, new int[0], i)).ToArray());
+                }
+
+                yield return res;
+                yield break;
+            }
+
+            var fs = GetTransformedFixed(direction);
+            foreach (var f in fs)
+            {
+                var res = new List<int[]>();
+                for (var i = 0; i < dimention; i++)
+                {
+                    res.Add(direction.Select((d, di) => TransformDirection(direction, di, dimention, f, i)).ToArray());
+                }
+                yield return res;
+            }
+        }
+
+        private static int TransformDirection(int[] direction, int di, int dimention, int[] f, int i)
+        {
+            switch (direction[di])
+            {
+                case -1:
+                    return dimention - 1 - i;
+
+                case 0:
+                    return f[direction.Take(di).Count(x => x == 0)];
+
+                case 1:
+                    return 0 + i;
+            }
+
+            throw new Exception("Something went wrong");
+        }
+
+        public static IEnumerable<int[]> GetTransformedFixed(int[] direction)
+        {
+            var freedom = direction.Count(x => x == 0);
+            var dimention = direction.Length;
+            return FixedTransformations(freedom, dimention);
+        }
+
+        private static IEnumerable<int[]> FixedTransformations(int freedom, int dimention)
+        {
+            if (freedom == 0) return new int[0][];
+
+            var range = Enumerable.Range(0, dimention).ToArray();
+            if (freedom == 1) return range.Select(r => new[] { r });
+
+            var fs = FixedTransformations(freedom - 1, dimention).ToArray();
+            return range.SelectMany(r => fs.Select(f => f.Concat(new[] { r }).ToArray()));
+        }
+
+        public static IEnumerable<int[]> GetDirections(int dimention)
+        {
+            return GetDirectionsHelper(dimention).TakeWhile(x => x.Any(i => i != 0));
+        }
+
+        private static IEnumerable<int[]> GetDirectionsHelper(int dimention)
+        {
+            if (dimention == 1)
+            {
+                return Enumerable.Range(-1, 3).Select(x => new[] { x });
+            }
+
+            var r = GetDirectionsHelper(dimention - 1);
+            return Enumerable.Range(-1, 3).SelectMany(x => r.Select(r2 => r2.Concat(new[] { x }).ToArray()));
+        }
+    }
+
+}
---
+++ src/examples/generic-grid/Faze.Examples.GridGames/OXnState.cs
@@ -0,0 +1,126 @@
+﻿using Faze.Abstractions.GameMoves;
+using Faze.Abstractions.GameResults;
+using Faze.Abstractions.GameStates;
+using Faze.Abstractions.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Faze.Examples.GridGames
+{
+    public struct OXnState : IGameState<GridMove, WinLoseDrawResult?>
+    {
+        private int MaxMoves => Dimention * Dimention;
+        private readonly ulong[] WinningStates;
+
+        private List<GridMove> availableMoves;
+        private ulong p1Moves;
+        private ulong p2Moves;
+        private bool p1Turn;
+
+        public OXnState(int n) : this(n, CalcWinningStates(n).ToArray())
+        {
+        }
+
+        public OXnState(int n, ulong[] winningStates) : this(n, 0, 0, true, Enumerable.Range(0, n * n).Select(x => new GridMove(x)).ToList(), winningStates)
+        {
+        }
+
+        private OXnState(int n, ulong p1Moves, ulong p2Moves, bool p1Turn, List<GridMove> availableMoves, ulong[] winningStates)
+        {
+            Dimention = n;
+            WinningStates = winningStates;
+
+            this.p1Turn = p1Turn;
+            this.availableMoves = availableMoves.ToList();
+            this.p1Moves = p1Moves;
+            this.p2Moves = p2Moves;
+        }
+
+        public int Dimention { get; }
+        public int TotalPlayers => 2;
+        public PlayerIndex CurrentPlayerIndex => p1Turn ? PlayerIndex.P1 : PlayerIndex.P2;
+
+        public IEnumerable<GridMove> GetAvailableMoves()
+        {
+            return availableMoves;
+        }
+
+        public IGameState<GridMove, WinLoseDrawResult?> Move(GridMove move)
+        {
+            ulong bitwiseMove = 1uL << move;
+            var newP1Moves = p1Moves;
+            var newP2Moves = p2Moves;
+            var newAvailableMoves = availableMoves.ToList();
+
+            if (p1Turn)
+            {
+                newP1Moves |= bitwiseMove;
+            }
+            else
+            {
+                newP2Moves |= bitwiseMove;
+            }
+
+            newAvailableMoves.Remove(move);
+
+            return new OXnState(Dimention, newP1Moves, newP2Moves, !p1Turn, newAvailableMoves, WinningStates);
+        }
+
+        public WinLoseDrawResult? GetResult()
+        {
+            // minimum of (Dimention * 2 - 1) moves is required to end the game
+            if (MaxMoves - availableMoves.Count < Dimention * 2 - 1) return null;
+
+            foreach (var state in WinningStates)
+            {
+                if ((p1Moves & state) == state) return WinLoseDrawResult.Win;
+                if ((p2Moves & state) == state) return WinLoseDrawResult.Lose;
+            }
+
+            return availableMoves.Count == 0 ? WinLoseDrawResult.Draw : (WinLoseDrawResult?)null;
+        }
+
+        public static IEnumerable<ulong> CalcWinningStates(int dimention)
+        {
+            // horizontal
+            ulong mask = (ulong)Math.Pow(2, dimention) - 1;
+            for (var j = 0; j < dimention; j++)
+            {
+                yield return mask << j * dimention;
+            }
+
+            //vertical
+            mask = 0;
+            for (var i = 0; i < dimention; i++)
+            {
+                mask = mask << dimention;
+                mask += 1;
+            }
+
+            for (var i = 0; i < dimention; i++)
+            {
+                yield return mask << i;
+            }
+
+            mask = 0;
+            for (var i = 0; i < dimention; i++)
+            {
+                mask |= 1uL << i * (dimention + 1);
+            }
+
+            yield return mask;
+
+            mask = 0;
+            for (var i = 0; i < dimention; i++)
+            {
+                mask |= 1uL << i * (dimention - 1);
+            }
+
+            mask = mask << dimention - 1;
+
+            yield return mask;
+        }
+    }
+}
