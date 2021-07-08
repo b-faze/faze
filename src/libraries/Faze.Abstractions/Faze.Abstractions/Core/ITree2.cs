afcaf2f695ccf09936a7c0c2efb36ca28b8f2aef
b-hub <ben.magistris@gmail.com>
2021-07-08 12:31:07 +0100

    Add ITree2 to see if it might be worth using ITree interfaces - probably not

---
+++ src/libraries/Faze.Abstractions/Faze.Abstractions/Core/ITree2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Faze.Abstractions.Core
+{
+    //public interface ITree2<T>
+    //{
+    //    IEnumerable<T> Children { get; }
+    //}
+
+    //public interface ITree2<TTree, TValue> : ITree2<TTree>
+    //{
+    //    TValue Value { get; }
+    //}
+
+    //public class EnumerableTree<TValue> : ITree2<EnumerableTree<TValue>, TValue>
+    //{
+    //    public EnumerableTree(TValue value, IEnumerable<EnumerableTree<TValue>> children)
+    //    {
+    //        Value = value;
+    //        Children = children;
+    //    }
+
+    //    public TValue Value { get; }
+
+    //    public IEnumerable<EnumerableTree<TValue>> Children { get; }
+    //}
+}
