aa1a8c434dfb9b614539e263befa724144c8d312
b-hub <ben.magistris@gmail.com>
2021-07-14 09:36:25 +0100

    Add Faze.Abstractions.Tests - initial project

---
+++ src/libraries/Faze.Abstractions/Faze.Abstractions.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Faze.Abstractions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // test
+        }
+    }
+}
