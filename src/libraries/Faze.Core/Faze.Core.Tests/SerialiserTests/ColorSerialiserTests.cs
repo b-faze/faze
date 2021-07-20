82dbf1243e4ce6d9f5c4bf9e8369efac365d7e04
b-hub <ben.magistris@gmail.com>
2021-07-20 14:47:30 +0100

    add serialiser tests

---
+++ src/libraries/Faze.Core/Faze.Core.Tests/SerialiserTests/ColorSerialiserTests.cs
@@ -0,0 +1,42 @@
+﻿using Faze.Core.Serialisers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace Faze.Core.Tests.SerialiserTests
+{
+    public class ColorSerialiserTests
+    {
+        [Fact]
+        public void CanWriteAndRead()
+        {
+            var colors = new[]
+            {
+                Color.Red,
+                Color.Green,
+                Color.Blue,
+                Color.White,
+                Color.Black,
+                Color.Aqua
+            };
+            var serialiser = new ColorSerialiser();
+
+
+            foreach (var originalColor in colors)
+            {
+                var actualColor = serialiser.Deserialize(serialiser.Serialize(originalColor));
+
+                actualColor.A.ShouldBe(originalColor.A);
+                actualColor.R.ShouldBe(originalColor.R);
+                actualColor.G.ShouldBe(originalColor.G);
+                actualColor.B.ShouldBe(originalColor.B);
+            }
+
+        }
+    }
+}
---
+++ src/libraries/Faze.Core/Faze.Core.Tests/SerialiserTests/NullableIntSerialiserTests.cs
@@ -0,0 +1,34 @@
+﻿using Faze.Core.Serialisers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Shouldly;
+
+namespace Faze.Core.Tests.SerialiserTests
+{
+    public class NullableIntSerialiserTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        public void CanWriteAndRead(int? originalValue)
+        {
+            var serialiser = new NullableIntSerialiser();
+
+            var actualValue = serialiser.Deserialize(serialiser.Serialize(originalValue));
+
+            actualValue.ShouldBe(originalValue);
+
+        }
+    }
+}
