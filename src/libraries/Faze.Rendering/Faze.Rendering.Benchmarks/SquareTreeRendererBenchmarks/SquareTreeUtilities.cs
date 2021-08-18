c5ca905359fd05e67f8d5d714304e7348035afb2
b-hub <ben.magistris@gmail.com>
2021-08-18 13:26:39 +0100

    refactor square grid utilities and tree utilities

--- src/libraries/Faze.Rendering/Faze.Rendering.Benchmarks/SquareTreeRendererBenchmarks/SquareTreeUtilities.cs
+++ src/libraries/Faze.Rendering/Faze.Rendering.Benchmarks/SquareTreeRendererBenchmarks/SquareTreeUtilities.cs
@@ -9,26 +9,26 @@ using System.Drawing;
 
 namespace Faze.Rendering.Benchmarks.SquareTreeRendererBenchmarks
 {
-    public static class SquareTreeUtilities
-    {
-        public static Tree<Color> CreateGreyPaintedSquareTree(int size, int maxDepth, int depth = 0)
-        {
-            var tree = CreateSquareTree(size, maxDepth, depth)
-                .MapValue((v, info) => info.Depth)
-                .MapValue(v => (int)(255 * (1 - (double)v / maxDepth)))
-                .MapValue(v => Color.FromArgb(v, v, v));
+    //public static class SquareTreeUtilities
+    //{
+    //    public static Tree<Color> CreateGreyPaintedSquareTree(int size, int maxDepth, int depth = 0)
+    //    {
+    //        var tree = CreateSquareTree(size, maxDepth, depth)
+    //            .MapValue((v, info) => info.Depth)
+    //            .MapValue(v => (int)(255 * (1 - (double)v / maxDepth)))
+    //            .MapValue(v => Color.FromArgb(v, v, v));
 
-            return new Tree<Color>(tree.Value, tree.Children);
-        }
+    //        return new Tree<Color>(tree.Value, tree.Children);
+    //    }
 
-        private static Tree<int> CreateSquareTree(int size, int maxDepth, int depth = 0)
-        {
-            if (depth == maxDepth)
-                return new Tree<int>(depth);
+    //    private static Tree<int> CreateSquareTree(int size, int maxDepth, int depth = 0)
+    //    {
+    //        if (depth == maxDepth)
+    //            return new Tree<int>(depth);
 
-            var children = Enumerable.Range(0, size * size).Select(i => CreateSquareTree(size, maxDepth, depth + 1));
+    //        var children = Enumerable.Range(0, size * size).Select(i => CreateSquareTree(size, maxDepth, depth + 1));
 
-            return new Tree<int>(depth, children);
-        }
-    }
+    //        return new Tree<int>(depth, children);
+    //    }
+    //}
 }
