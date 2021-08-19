01c4717f65e3dba784345b8c88ebd50a6e81e78d
b-hub <ben.magistris@gmail.com>
2021-08-19 08:56:24 +0100

    Update WinLoseDrawResultAggregateTests.cs

--- src/libraries/Faze.Abstractions/Faze.Abstractions.Tests/WinLoseDrawResultAggregateTests.cs
+++ src/libraries/Faze.Abstractions/Faze.Abstractions.Tests/WinLoseDrawResultAggregateTests.cs
@@ -95,6 +95,52 @@ namespace Faze.Abstractions.Tests
                 results.Add(unknownResult);
             });
         }
+
+        [Fact]
+        public void CanCreateFromWinLoseDrawResult()
+        {
+            new WinLoseDrawResultAggregate(WinLoseDrawResult.Draw).Draws.ShouldBe((uint)1);
+            new WinLoseDrawResultAggregate(WinLoseDrawResult.Win).Wins.ShouldBe((uint)1);
+            new WinLoseDrawResultAggregate(WinLoseDrawResult.Lose).Loses.ShouldBe((uint)1);
+        }
+
+        [Fact]
+        public void CanAggregateWithInterfaceValue()
+        {
+            IResultAggregate<WinLoseDrawResultAggregate> agg = new WinLoseDrawResultAggregate();
+            agg.Value.Add(WinLoseDrawResult.Win);
+        }
+
+        [Fact]
+        public void CanGetWinsOverLoses()
+        {
+            var agg = new WinLoseDrawResultAggregate();
+            agg.Add(WinLoseDrawResult.Win);
+            agg.Add(WinLoseDrawResult.Win);
+            agg.Add(WinLoseDrawResult.Lose);
+            agg.Add(WinLoseDrawResult.Lose);
+
+            agg.GetWinsOverLoses().ShouldBeLessThanOrEqualTo(0.5);
+            agg.GetWinsOverLoses().ShouldBeGreaterThanOrEqualTo(0.5);
+        }
+
+        [Fact]
+        public void CanCompareWithEquals()
+        {
+            var agg1 = new WinLoseDrawResultAggregate(1, 2, 3);
+            var agg2 = new WinLoseDrawResultAggregate(1, 2, 3);
+
+            agg1.Equals(agg2).ShouldBeTrue();
+            agg1.Equals(0).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void CorrectToString()
+        {
+            var agg = new WinLoseDrawResultAggregate(1, 2, 3);
+
+            agg.ToString().ShouldBe("1,2,3");
+        }
     }
 
 }
