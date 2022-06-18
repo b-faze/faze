b4bf84bad3560c16aa2c5a3fb2bd4c4183994d7e
b-hub <ben.magistris@gmail.com>
2022-06-18 16:16:39 +0100

    initial API project

---
+++ src/examples/gallery/Faze.Examples.Gallery.API/Controllers/GalleryImageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Faze.Examples.Gallery.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GalleryImageController : ControllerBase
+    {
+        private readonly ILogger<GalleryImageController> _logger;
+
+        public GalleryImageController(ILogger<GalleryImageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<GalleryImage> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new GalleryImage
+            {
+                Width = 100,
+                Height = 100,
+                Color = "FF0000"
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ src/examples/gallery/Faze.Examples.Gallery.API/GalleryImage.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Faze.Examples.Gallery.API
+{
+    public class GalleryImage
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Color { get; set; }
+    }
+}
